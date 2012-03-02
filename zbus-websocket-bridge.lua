7e49d70297874edb6269d728d039dcedaa741172
Gerhard Lipp <gerhard_lipp@gmx.de>
2012-03-02 09:59:48 +0100

    added websocket bridge

---
+++ zbus-websocket-bridge.lua
@@ -0,0 +1,92 @@
+local cjson = require'cjson'
+local ev = require'ev'
+local websockets = require'websockets'
+local zbus = require'zbus'
+local file_dir = arg[1] or './'
+local ws_ios = {}
+local context = nil
+local zbus = require'zbus'
+local zbus_config = require'zbus.json'
+zbus_config.name = 'jet.websocket'
+zbus_config.ev_loop = ev.Loop.default
+zbus_config.exit = 
+   function()
+      for fd,io in pairs(ws_ios) do
+	 io:stop(ev.Loop.default)
+      end
+      context:destroy()
+   end
+local m = zbus.member(zbus_config)
+
+context = websockets.context{
+   port = arg[2] or 8002,
+   on_add_fd = 
+      function(fd)	
+	 local io = ev.IO.new(
+	    function()
+	       context:service(0)
+	    end,fd,ev.READ)
+	 ws_ios[fd] = io
+	 io:start(ev.Loop.default)
+      end,
+   on_del_fd = 
+      function(fd)
+	 ws_ios[fd]:stop(ev.Loop.default)
+	 ws_ios[fd] = nil
+      end,
+   on_http = 
+      function(ws,uri)
+	 if uri == '/favicon.ico' then
+	    ws:serve_http_file(file_dir..'favicon.ico','image/x-icon')
+	 elseif uri == '/' or uri == '/index.html' then
+	    ws:serve_http_file(file_dir..'index.html','text/html')
+	 else
+	    local content_type
+	    if uri:match('css$') then
+	       content_type = 'text/css'
+	    elseif uri:match('less$') then
+	       content_type = 'text/less'
+	    elseif uri:match('html$') then
+	       content_type = 'text/html'
+	    elseif uri:match('js$') then
+	       content_type = 'text/javascript'
+	    end
+	    assert(content_type,'unknown content type:'..uri)
+	    ws:serve_http_file(file_dir..uri,content_type)
+	 end
+      end,
+   protocols = {
+      ['zbus-call'] =
+         function(ws)	  
+            ws:on_receive(
+      	       function(ws,data)
+		  local req = cjson.decode(data)
+		  local resp = {id=req.id}
+		  local result = {pcall(m.call,m,req.method,unpack(req.params))}
+		  if result[1] then 
+		     table.remove(result,1);
+		     resp.result = result
+		  else
+		     resp.error = result[2]
+		  end
+      		  ws:write(cjson.encode(resp),websockets.WRITE_TEXT)
+      	       end)
+         end,
+      ['zbus-notification'] =
+         function(ws)	  
+         end
+   }
+}
+
+m:listen_add(
+   '.*',
+   function(method,...)
+      local req = {}
+      req.method = method
+      req.params = {...}
+      context:broadcast('zbus-notification',cjson.encode(req))
+   end)
+
+m:loop()
+
+
