8c57703d1ec511ed464525ba5e56ec57dab155ec
Gerhard Lipp <gelipp@googlemail.com>
2012-06-26 10:20:36 +0200

    make variable echo args

--- examples/echo_client.lua
+++ examples/echo_client.lua
@@ -2,9 +2,10 @@ local zm = require'zbus.member'
 local member = zm.new()
 -- call the service function
 for i=1,arg[1] or 1 do
+   local text = 'hello'..tostring(math.random(1,1000000))
   local result_str = member:call(
     'echo', -- the method url/name
-    'Hello there' -- the argument string
+    text -- the argument string
   )
-  assert(result_str == 'Hello there')
+  assert(result_str == text)
 end
